dffef209b093f848a424b5c729a8bfb9d7741d72
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-01-30 11:56:47 +0100

    adding html attribute props

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -7,7 +7,7 @@ const MainHeader = (props) => {
     return (
         <div className={styles.mainHeader}>
             <h1 className={styles.headerText}>A Typical Page</h1>
-            <NavBar />
+            <NavBar isLoging={props.onAuthentication} onLogout={props.onLogout}/>
         </div>
     )
 }
