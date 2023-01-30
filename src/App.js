36a6511aba7ee20d8800aed85838816457ffd757
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-01-30 11:50:04 +0100

    making home page show when u login n vice versa

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Home from './components/Home/Home';
 import Logins from './components/Logins/Logins';
 import MainHeader from './components/MainHeader/MainHeader';
 
 
+
 function App() {
+
+  const [isLoging, setIsloging] = useState(false)
+
+  const logoutHandler = () => {
+    setIsloging(false)
+  }
+
+  const loginHandler = () => {
+    setIsloging(true)
+  }
+
+
+
+
   return (
     <Fragment>
-      <MainHeader />
-      {/* <Home /> */}
-      <Logins />
+      <MainHeader onAuthentication={isLoging} onLogout={logoutHandler} />
+      {isLoging && <Home />}
+      {!isLoging && <Logins onLogin={loginHandler}/>}
     </Fragment>
   );
 }
