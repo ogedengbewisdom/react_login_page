e25aad617eed7824fa883a39d3547300d8e689c0
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-03-22 08:21:05 +0100

    firebase configuration file

---
+++ src/Databease/firebaseConfig.js
@@ -0,0 +1,12 @@
+
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAncZAZMHOXv5ilFFh3mzF769ciYQpLNZc",
+  authDomain: "reactloginauthentication.firebaseapp.com",
+  projectId: "reactloginauthentication",
+  storageBucket: "reactloginauthentication.appspot.com",
+  messagingSenderId: "969265743653",
+  appId: "1:969265743653:web:29ffb8b900725b8495bccc"
+};
+
+export const app = initializeApp(firebaseConfig);
