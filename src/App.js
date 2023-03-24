d2e85641879ac74d5d6fd25c252fd395fe666409
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-03-24 07:41:45 +0100

    handling logout auth

--- src/App.js
+++ src/App.js
@@ -3,19 +3,50 @@ import Home from './components/Home/Home';
 import Logins from './components/Logins/Logins';
 import MainHeader from './components/MainHeader/MainHeader';
 import { app } from "./Databease/firebaseConfig"
-
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 
 function App() {
-
+  const auth = getAuth()
   const [isLoging, setIsloging] = useState(false)
 
-  const logoutHandler = () => {
-    setIsloging(false)
+  const loginHandler = (email, password) => {
+    
+    signInWithEmailAndPassword(auth, email, password )
+    .then((response) => {
+      console.log(response.user)
+      setIsloging(true)
+    })
+    .catch((error) => {
+      alert(error.message)
+      setIsloging(false)
+    })
+    
   }
 
-  const loginHandler = () => {
-    setIsloging(true)
+  
+  const logoutHandler = async () => {
+
+    try {
+      await signOut(auth)
+
+      console.log(signOut(auth))
+      setIsloging(false) 
+    }
+
+    catch (error) {
+      alert(error.message)
+      setIsloging(true)
+    }
+
+    // signOut(auth)
+    // .then(() => {
+    //   setIsloging(false)
+    // })
+    // .then((error) => {
+    //   alert(error.message)
+    //   setIsloging(true)
+    // })
   }
 
 
